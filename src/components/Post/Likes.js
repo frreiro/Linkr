37fe218ac9550ad3a60c31140f02cfddd9f13674
marcojr73 <marcojuniorengineer@gmail.com>
2022-06-19 22:35:50 -0300

    visualização sobre quem curtiu

--- src/components/Post/Likes.js
+++ src/components/Post/Likes.js
@@ -28,12 +28,14 @@ export default function Like({postId}){
         let userName = data.name
         try {
             const promisse = await axios.get(`${url}/${postId}`, config)
-            console.log(promisse.data)
-            setLikedBy(promisse.data)
-            if(promisse.data[0].userName === userName){
-                setLike(liked)
+            for(let i = 0; i<promisse.data.length; i ++){
+                if(promisse.data[i] === userName){
+                    setLike(liked)
+                }
             }
+            setLikedBy(filter(promisse.data))
         } catch (error) {
+            alert("ocorreu um erro")
         }
     }
 
@@ -41,11 +43,22 @@ export default function Like({postId}){
         getlist()
     },[])
 
+    function filter(names){
+        for(let i = 0; i<names.length; i++){
+            if(names[i]===data.name){
+                names.unshift("você")
+            }
+        }
+        if(names.length === 0) return null
+        else if (names.length ===1) return `${names[0]} curtiu`
+        else if (names.length === 2) return `${names[0]} e ${names[1]} curtiram`
+        else if (names.length > 2) return `${names[0]} e ${names[1]} e outros ${names.length - 2} curtiram`
+    }
+
     async function action(postId){
-        console.log(likedBy)
+        // console.log(likedBy)
         try {
             const promisse = await axios.post(url, {postId}, config)
-            getlist()
             if(like === unLike){
                 setLike(liked)
             } else {
@@ -58,11 +71,16 @@ export default function Like({postId}){
 
     return(
         <Container>
-            <img data-tip onClick={()=> action(postId)} className='like' src={like}/>
+            <img data-tip="" data-for={String(postId)} onClick={()=> action(postId)} className='like' src={like}/>
             <p>{likesCount ? `${likesCount} likes` : `0 likes`}</p>
-            <ReactTooltip getContent={()=>{
-                return 
-            }}/>
+            <ReactTooltip
+              id={String(postId)}
+              getContent={() => {
+                return null
+              }}
+            >
+              {likedBy && <span>{likedBy}</span>}
+            </ReactTooltip>
         </Container>
     )
 }
